971edff5a020ea0b8f6c0d129159ae8506566df8
Enderoc <95502989+Enderoc@users.noreply.github.com>
2022-03-30 15:32:18 +0500

    Add files via upload

---
+++ Laba1.cpp
@@ -0,0 +1,32 @@
+﻿#include <GL/freeglut.h>
+
+static void RenderSceneCB() //Функция рендера
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+static void GlutCallbacks()
+{
+	glutDisplayFunc(RenderSceneCB); //отрисовка glut'ом 1 кадра с окном
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv); //инициализируем glut
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // настраиваем некоторые опции glut
+
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 01"); //Параметры окна
+
+	glutDisplayFunc(RenderSceneCB); //присоединяем функцию RenderSceneCB к glut
+
+	glClearColor(0.0f, 0.0f, 1.0f, 0.0f); //цвет фона окна, который будет использован во время очистки буфера кадра
+	glutMainLoop(); //передаём контроль GLUT'у
+
+	glClear(GL_COLOR_BUFFER_BIT); //очистка буфер кадра
+	glutSwapBuffers(); //меняем фоновый буфер и буфер кадра местами
+
+	return 0;
+}
